5115d46d26d1cdc99cc8a02ad367fea1fdfff1ce
beckytieman45 <beckytieman@gmail.com>
2021-09-23 17:59:25 -0700

    change node issues

---
+++ app.js
@@ -0,0 +1,41 @@
+
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 3000; 
+
+
+const app = express();
+
+
+//Routes
+const productRoutes = require('./routes/add-product');
+const shopRoutes = require('./routes/shop');
+
+//template engine
+app
+  .use(express.static(path.join(__dirname, 'public'))) //css
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .use(bodyParser.urlencoded({extended: false}))
+  .use(productRoutes.routes)
+//   .get('/',(req, res, next) => {
+    
+//     //console.log(adminData.products);
+//     //res.sendFile(path.join(rootDir, 'week02-views', 'shop.html'));
+//     res.render('shop', {
+//         prods: products, 
+//         pageTitle: 'E-Commerce Shop', 
+//         path: '/', hasProducts: 
+//         products.length > 0,
+//         activeShop: true,
+//         productCSS: true
+//         });
+//     })
+    .use(shopRoutes)
+   
+   
+app.use((req, res, next) => {
+    res.status(404).render('404', {pageTitle: 'Page Not Found!'});
+    })
+app.listen(PORT);
